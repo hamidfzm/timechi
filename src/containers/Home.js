454dc62f2b40bcb931ad40d1344fda4eccf4d3c9
Hamid FzM <hamidfzm@gmail.com>
2018-01-19 13:04:04 +0330

    Implement home page component

---
+++ src/containers/Home.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {Jumbotron, Container, Row, Col} from 'reactstrap';
+
+import Navbar from './Navbar';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar/>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Jumbotron>
+                                <h1 className='display-3'>Timechi</h1>
+                                <p className='lead'>Timechi is a time tracking service.</p>
+                            </Jumbotron>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Home;
