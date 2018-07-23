222e667a94c389d4ec1ea44f9ce680e3f6a115e8
Hamid FzM <hamidfzm@gmail.com>
2018-07-23 17:25:58 +0430

    Update login form
    Use common form field component

--- src/containers/forms/Login.js
+++ src/containers/forms/Login.js
@@ -2,57 +2,35 @@ import React, {Component} from 'react';
 import {
     Button,
     Form,
-    FormGroup,
-    Label,
-    Input,
-    FormFeedback,
 } from 'reactstrap';
 import {Field, reduxForm} from 'redux-form';
 import PropTypes from 'prop-types';
 
 import * as validations from './validations';
+import CommonFormField from '../../components/CommonFormField';
 
 class CustomForm extends Component {
-    renderField = ({
-                       name,
-                       input,
-                       label,
-                       placeholder,
-                       type,
-                       meta: {touched, error},
-                   }) => {
-        return (
-            <FormGroup>
-                <Label for={name}>{label}</Label>
-                <Input valid={!(error)}  {...input} placeholder={placeholder} type={type}/>
-                {
-                    touched && error &&
-                    <FormFeedback>{error}</FormFeedback>
-                }
-            </FormGroup>
-
-
-        );
-    };
-
     render() {
-        const {submitting, onSubmit} = this.props;
+        const {submitting, handleSubmit} = this.props;
 
         return (
-            <Form noValidate onSubmit={onSubmit}>
-                <Field name='email'
-                       type='email'
-                       component={this.renderField}
-                       placeholder='Input your email address'
-                       label='Email'
+            <Form noValidate onSubmit={handleSubmit}>
+                <Field name={'email'}
+                       type={'email'}
+                       component={CommonFormField}
+                       placeholder={'Input your email address'}
+                       label={'Email'}
+                       disabled={submitting}
+                       returnKeyType={'next'}
                        validate={[validations.required, validations.email]}/>
-                <Field name='password'
-                       type='password'
-                       component={this.renderField}
-                       placeholder='Input your password'
-                       label='Password'
+                <Field name={'password'}
+                       type={'password'}
+                       component={CommonFormField}
+                       placeholder={'Input your password'}
+                       label={'Password'}
+                       disabled={submitting}
                        validate={[validations.required, validations.minLength(6)]}/>
-                <Button disabled={submitting} type='submit' color='primary' block>Submit</Button>
+                <Button disabled={submitting} type={'submit'} color={'primary'} block>Submit</Button>
             </Form>
         );
     }
